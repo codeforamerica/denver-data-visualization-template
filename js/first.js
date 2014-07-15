62cf91f8d5c3ddeb3307a6f4685447966901dadd
Becky Boone <becky.sue.boone@gmail.com>
2014-07-15 15:45:15 -0700

    Populate big number from csv

---
+++ js/first.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+  populateBigNumber("first");
+});
---
+++ js/main.js
@@ -0,0 +1,11 @@
+function populateBigNumber(id) {
+  var csvFile = "data/" + id + "-big.csv";
+  d3.csv(csvFile, function(error, data) {
+    if(error){
+      console.log(error);
+    }
+    else {
+	   $("#" + id + "-number").text(data[0].big);
+    }
+  });
+}
